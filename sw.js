b7190ac496f97704829bce23b97e3287a8fbf886
franckgrimaud013-ui <franckgrimaud013@gmail.com>
2025-11-05 08:09:50 +0100

    Refactor sw.js to minimal PWA service worker
    Simplified service worker to minimal implementation for installable PWA.

--- sw.js
+++ sw.js
@@ -1,24 +1,3 @@
-const CACHE = "franck-40ans-v1";
-const ASSETS = [
-  "./",
-  "./index-4-pwa.html",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match("./index-4-pwa.html")))
-  );
-});
+// sw.js minimal pour PWA installable\ nself.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', () => {});
