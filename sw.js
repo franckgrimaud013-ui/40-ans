8706207e1bffe62b563b5efc0af11fffaa49182b
franckgrimaud013-ui <franckgrimaud013@gmail.com>
2025-11-05 10:08:31 +0100

    Remove comment from service worker file

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// sw.js minimal pour PWA installable\ nself.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => self.clients.claim());
 self.addEventListener('fetch', () => {});
